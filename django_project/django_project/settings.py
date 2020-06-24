e5ae34ff58a71da7989f7f418bff5b314f7d20d7
aadideshpande <aadi.deshpande1@gmail.com>
2020-06-24 19:32:12 +0530

    cripy_forms library used

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'blog',
     'users',
+    'crispy_forms',
+
 ]
 
 MIDDLEWARE = [
@@ -120,3 +122,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
--- django_project/users/views.py
+++ django_project/users/views.py
@@ -2,20 +2,21 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.http import HttpResponse
 from django.contrib import messages
+from .forms import UserRegisterForm
 
 # Create your views here.
 def register(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = UserRegisterForm(request.POST)
+        print(form)
         if form.is_valid():
+            form.save()
             username = form.cleaned_data.get('username')
             messages.success(request, f'Account created for {username}.')
             return redirect('home')
-        else:
-            messages.info(request, f'Something is wrong')
-            return redirect('register')   
-
-
+        #else:
+            #messages.info(request, f'Enter the information according to the instructions')
+            #return redirect('register')   
     else:
-        form = UserCreationForm()
-        return render(request, 'users/register.html', {'form' : form})
+        form = UserRegisterForm()
+    return render(request, 'users/register.html', {'form' : form})
