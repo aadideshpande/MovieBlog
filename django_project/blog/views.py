9c3fb44caa0bf1484df299c46fa5612a50a0069d
aadideshpande <aadi.deshpande1@gmail.com>
2020-06-24 10:41:37 +0530

    boostrap changes

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,8 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
+
+posts = [
+    {
+        'title' : "Oscars delayed",
+        'author': "aadi",
+        'text'  : "Oscars have been delayed"
+    },
+    {
+        'title' : "tenet huly 31st",
+        'author': "aadi",
+        'text'  : "Nolan's new film"
+    }
+]
+
+contexts = {
+    'posts' : posts
+}
 def home(request):
-    return render(request, 'blog/home.html')
+   
+    return render(request, 'blog/home.html', contexts)
 
 def about(request):
     return render(request, 'blog/about.html')
