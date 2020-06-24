1bb9c84cb88e6f2777b70ec775c26e305cbba69f
aadideshpande <aadi.deshpande1@gmail.com>
2020-06-24 08:24:30 +0530

    created main html files

--- django_project/blog/urls.py
+++ django_project/blog/urls.py
@@ -2,5 +2,6 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('blog', views.home, name="home")
+    path('blog', views.home, name="home"),
+    path('about', views.about, name="about"),
 ]
