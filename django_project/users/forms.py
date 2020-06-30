58922b15a6b969d264f8b9c7e2be8c7ea7531373
aadideshpande <aadi.deshpande1@gmail.com>
2020-06-30 22:31:18 +0530

    profile update feature done

--- django_project/users/forms.py
+++ django_project/users/forms.py
@@ -1,11 +1,21 @@
 from django import forms  
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
-
+from .models import Profile
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField()
 
     class Meta:
-        model = User
-        fields = ['username', 'email', 'password1', 'password2']
+        model = User #whenever the form validates, we create a new user
+        fields = ['username', 'email', 'password1', 'password2']    #these are to be shown on the form
 
+
+class UserUpdateForm(forms.ModelForm):
+    class Meta:
+        model = User  # images are in Profile model , so we need to do it differently
+        fields = ['username', 'email'] 
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['image']
