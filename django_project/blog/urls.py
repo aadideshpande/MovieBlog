35bfc06abc8e09c6a0fcba6d6ad38505d25a5cb2
aadideshpande <aadi.deshpande1@gmail.com>
2020-07-01 12:21:10 +0530

    posts update, create, delete done

--- django_project/blog/urls.py
+++ django_project/blog/urls.py
@@ -1,7 +1,16 @@
 from django.contrib import admin
 from django.urls import path
+from .views import (PostListView, 
+                    PostDetailView,
+                    PostCreateView,
+                    PostUpdateView,
+                    PostDeleteView)
 from . import views
 urlpatterns = [
-    path('blog', views.home, name="home"),
+    path('', PostListView.as_view(), name="home"),
     path('about', views.about, name="about"),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('post/new/', PostCreateView.as_view(), name='post-create'),
+    path('post/<int:pk>/update', PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete', PostDeleteView.as_view(), name='post-delete'),
 ]
